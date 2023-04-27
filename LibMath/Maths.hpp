5d2e6b94f34a73ae88a035b21baef68eaa7a2a12
m.robion <m.robion@student.isartdigital.com>
2023-04-27 11:38:54 +0200

    [update] Vec2 members turn in union

--- LibMath/Maths.hpp
+++ LibMath/Maths.hpp
@@ -30,7 +30,14 @@ namespace Maths
     {
     public:
         //MEMBERS
-        float x, y;
+        union
+        {
+            struct
+            {
+                float x, y;
+            };
+            float xy[2];
+        };
 
         //CONSTRUCTORS
         inline Vec2(void);
